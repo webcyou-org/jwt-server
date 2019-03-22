54f1cf8bb1cef179fb191c468a3b9644b2d7574c
panicdragon <d.takayama.web@gmail.com>
2019-03-23 00:05:08 +0900

    create users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,44 @@
 module Api
   module V1
     class UsersController < ApplicationController
+      before_action :authenticate_user, except: [:create, :logout]
+
+      def index
+        render json: {status: 200, user: response_fields(current_user.to_json) }
+      end
+
+      def create
+        @user = User.new(users_params)
+
+        if @user.name.blank?
+          response_bad_request
+        else
+          if User.exists?(email: @user.email)
+            response_conflict(:user)
+          else
+            if @user.save
+              response_success(:user, :create)
+            else
+              response_internal_server_error
+            end
+          end
+        end
+      end
+
+      def logout
+        render json: {status: 200}
+      end
+
+      private
+
+      def users_params
+        params.permit(:name, :email, :password)
+      end
+
+      def response_fields(user_json)
+        user_parse = JSON.parse(user_json)
+        user_parse.except('created_at', 'updated_at', 'password', 'password_digest')
+      end
     end
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,33 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
+  
+  # 200 Success
+  def response_success(class_name, action_name)
+    render status: 200, json: { status: 200, message: "Success #{class_name.capitalize} #{action_name.capitalize}" }
+  end
+
+  # 400 Bad Request
+  def response_bad_request
+    render status: 400, json: { status: 400, message: 'Bad Request' }
+  end
+
+  # 401 Unauthorized
+  def response_unauthorized
+    render status: 401, json: { status: 401, message: 'Unauthorized' }
+  end
+
+  # 404 Not Found
+  def response_not_found(class_name = 'page')
+    render status: 404, json: { status: 404, message: "#{class_name.capitalize} Not Found" }
+  end
+
+  # 409 Conflict
+  def response_conflict(class_name)
+    render status: 409, json: { status: 409, message: "#{class_name.capitalize} Conflict" }
+  end
+
+  # 500 Internal Server Error
+  def response_internal_server_error
+    render status: 500, json: { status: 500, message: 'Internal Server Error' }
+  end
 end
