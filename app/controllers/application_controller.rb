cab0ea595a609a7ae48e750045f9967eff79682a
panicdragon <d.takayama.web@gmail.com>
2019-03-23 00:10:04 +0900

    add routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
-  
+
   # 200 Success
   def response_success(class_name, action_name)
     render status: 200, json: { status: 200, message: "Success #{class_name.capitalize} #{action_name.capitalize}" }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'users' => 'users#index'
+      post 'users' => 'users#create'
+      delete 'users' => 'users#logout'
       post 'user_token' => 'user_token#create'
     end
   end
